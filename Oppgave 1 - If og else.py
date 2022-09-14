0031610de390483a34c9bb7fc1277ed57f17c63c
pollekillerxd <76852143+pollekillerxd@users.noreply.github.com>
2022-09-14 15:40:12 +0200

    Create Oppgave 1 - If og else.py

---
+++ Oppgave 1 - If og else.py
@@ -0,0 +1,10 @@
+
+print("Hva er meningen med livet?")
+Tall =int(input("skriv svaret på livet "))
+
+if (Tall) == "42":
+    print("Det stemmer, meningen med livet er 42!")   
+
+elif (Tall) <= "50" and Tall >= "30":
+    (print("Nærme, men feil."))
+
