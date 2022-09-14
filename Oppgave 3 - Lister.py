6da1dbf52ad2a75775e163f1402c41797228dcd2
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2022-09-14 21:51:30 +0200

    Create Oppgave 3 - Lister.py

---
+++ Oppgave 3 - Lister.py
@@ -0,0 +1,11 @@
+bøker = ["The Hobbit", "Farmer Giles of Ham", "The Fellowship of the Ring", "The Two Towers","The Return of the King" ,"The Adventures of Tom Bombadil" , "Tree and Leaf"]
+print(bøker[0:2])
+print(bøker[5:7])
+
+bøker.append("The Silmarillion")
+bøker.append("Unfinished Tales")
+
+bøker[2] ="Lord of the Rings The Fellowship of the Ring"
+bøker[3] ="Lord of the Rings The Two Towers"
+bøker[4] ="Lord of the Rings The Return of the King"
+
