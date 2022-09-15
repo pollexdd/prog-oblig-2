f405551122a39bec249447a3a5578d674d860f3c
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2022-09-16 00:06:25 +0200

    Update Oppgave 5 - for-løkke(r) - Dart.py

--- Oppgave 5 - for-løkke(r) - Dart.py
+++ Oppgave 5 - for-løkke(r) - Dart.py
@@ -13,6 +13,7 @@ if spillere == ("1"):
     print(poeng_pil_1+poeng_pil_2+poeng_pil_3)
 
 if spillere == ("2"):
-    for poeng_pil in range (3):
-        poeng = randrange(0, 60)
-        print(poeng)
+    poeng = 0
+    for poeng_pil in range (0, 3):
+        poeng_pil += poeng_pil
+    print(poeng_pil)
