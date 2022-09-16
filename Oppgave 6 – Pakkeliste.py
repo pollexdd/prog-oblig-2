8fe76ca8121734e4796e91a4ae811de13ff2a46f
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2022-09-16 16:53:46 +0200

    Update Oppgave 6 – Pakkeliste.py

--- Oppgave 6 – Pakkeliste.py
+++ Oppgave 6 – Pakkeliste.py
@@ -1,8 +1,7 @@
+pakkeliste = []
 
 while True:
 
-    pakkeliste = []
-    
     print("0 får å se listen")
     print("1 pc")
     print("2 klær")
@@ -18,6 +17,7 @@ while True:
         print(pakkeliste)
     
     if valg_av_modus == "6":
+        print("Da er du ferdig med å pakke dette har du tatt med deg " + str(pakkeliste))
         break
         
 
@@ -30,6 +30,46 @@ while True:
             pakkeliste.append("pc")
         if valg == "2":
             pakkeliste.remove("pc")
-            
+
+    if valg_av_modus == "2":
+        print("du har valgt klær, velg hva du vil gjøre")
+        valg = input("velg 1 får å legge til 2 for å slette fra liste ")
+        print("1 legg til")
+        print("2 slett")
+        if valg == "1": 
+            pakkeliste.append("klær")
+        if valg == "2":
+            pakkeliste.remove("klær")
+
+    if valg_av_modus == "3":
+        print("du har valgt sengetøy, velg hva du vil gjøre")
+        valg = input("velg 1 får å legge til 2 for å slette fra liste ")
+        print("1 legg til")
+        print("2 slett")
+        if valg == "1": 
+            pakkeliste.append("sengetøy")
+        if valg == "2":
+            pakkeliste.remove("sengetøy")
+
+    if valg_av_modus == "4":
+        print("du har valgt tannbørste, velg hva du vil gjøre")
+        valg = input("velg 1 får å legge til 2 for å slette fra liste ")
+        print("1 legg til")
+        print("2 slett")
+        if valg == "1": 
+            pakkeliste.append("tannbørste")
+        if valg == "2":
+            pakkeliste.remove("tannbørste")
+    
+    if valg_av_modus == "5":
+        print("du har valgt kontor stol, velg hva du vil gjøre")
+        valg = input("velg 1 får å legge til 2 for å slette fra liste ")
+        print("1 legg til")
+        print("2 slett")
+        if valg == "1": 
+            pakkeliste.append("kontor stol")
+        if valg == "2":
+            pakkeliste.remove("kontor stol")
+    
 
     
