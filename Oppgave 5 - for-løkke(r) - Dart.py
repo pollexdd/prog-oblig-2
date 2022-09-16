8849ea281c7f2b73a4ae6295d960cf945c213eb5
pollekillerxd <76852143+pollexdd@users.noreply.github.com>
2022-09-16 15:22:07 +0200

    Update Oppgave 5 - for-løkke(r) - Dart.py

--- Oppgave 5 - for-løkke(r) - Dart.py
+++ Oppgave 5 - for-løkke(r) - Dart.py
@@ -1,7 +1,11 @@
+
 from random import randrange
+import string
 
-poeng_pil = randrange(0, 60)
-
+sum1 = 0
+sum2 = 0
+sum3 = 0
+sum4 = 0
 print("velg hvor mange spillere mellom 1-4")
 
 spillere = input("velg et tall mellom 1-4 ") 
@@ -13,7 +17,10 @@ if spillere == ("1"):
     print(poeng_pil_1+poeng_pil_2+poeng_pil_3)
 
 if spillere == ("2"):
-    poeng = 0
-    for poeng_pil in range (0, 3):
-        poeng_pil += poeng_pil
-    print(poeng_pil)
+    for poeng_pil in range (3):
+        sum1 += randrange(0, 60)
+        print("spiller 1 fikk " + str(sum1))
+        continue
+    for poeng_pil in range (3):
+        sum2 += randrange(0, 60)
+        print(sum2)
